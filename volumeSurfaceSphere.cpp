e4229b55006e18a6b184325db952035292f5efab
Ubuntu <ubuntu@ip-172-31-40-13.ec2.internal>
2021-12-02 15:42:06 +0000

    fixed formating errors

--- volumeSurfaceSphere.cpp
+++ volumeSurfaceSphere.cpp
@@ -7,7 +7,6 @@
 // and displays the volume and surface area.
 #include <iostream>
 #include <cmath>
-using namespace std;
 
 // declare variables
 float radius, volume, surfaceArea;
@@ -17,26 +16,27 @@ int main() {
   // get input from user
   std::cout << "\033[1;31mToday we will calculate the volume and\033[0m\n";
   std::cout << "\033[1;31msurface area of a sphere.\033[0m\n";
-  std:: cout << "\033[1;31mEnter the units:\033[0m";
+  std::cout << "\033[1;31mEnter the units: \033[0m";
   std::cin >> units;
-  std:: cout << "\033[1;31mEnter the radius:\033[0m";
+  std::cout << "\033[1;31mEnter the radius: \033[0m";
   std::cin >> radius;
 
   // calculate surface area and volume
   volume = M_PI*4/3*pow(radius, 3);
   surfaceArea = 4*M_PI*pow(radius, 2);
-  
-  //round volume and surface area
+
+  // round volume and surface area
   volume = volume * 100;
   volume = round(volume);
   volume = volume / 100;
-  
+
   surfaceArea = surfaceArea * 100;
   surfaceArea = round(surfaceArea);
   surfaceArea = surfaceArea / 100;
 
   // display the results to the user
   std::cout << std::endl;
-  std::cout << "\033[;32mSurface area = " << surfaceArea << " " << units << "^2.\033[0m\n";
   std::cout << "\033[;32mVolume = " << volume << " " << units << "^3.\033[0m\n";
+  std::cout << "\033[;32mSurface area = " << surfaceArea << " "
+  << units << "^2.\033[0m\n";
 }
